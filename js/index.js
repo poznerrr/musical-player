8d7e6db5395832a582a29b3cba77d34858001eb6
poznerrr <pozner@gmail.com>
2022-11-07 21:22:11 +0300

    feat: add functionallity for play pause and stop player's buttons

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const audio = new Audio();
+const tracksCard = document.getElementsByClassName('track');
+const player = document.getElementsByClassName('player')[0];
+
+const pauseBtn = document.querySelector('.player__controller_pause');
+const stopBtn = document.querySelector('.player__controller_stop');
+
+const playMusic = (event) => {
+  const trackActive = event.currentTarget;
+  audio.src = trackActive.dataset.track;
+  audio.play();
+  pauseBtn.classList.remove('player__icon_play');
+  player.classList.add('player_active');
+
+  for (let i = 0; i < tracksCard.length; i++) {
+    tracksCard[i].classList.remove('track_active')
+  }
+
+  trackActive.classList.add('track_active');
+}
+
+const stopMusic = (event) => {
+  audio.src = null;
+  player.classList.remove('player_active');
+}
+
+for (let i = 0; i < tracksCard.length; i++) {
+  tracksCard[i].addEventListener('click', playMusic);
+}
+
+
+pauseBtn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    pauseBtn.classList.remove('player__icon_play');
+  } else {
+    audio.pause();
+    pauseBtn.classList.add('player__icon_play');
+  }
+})
+
+stopBtn.addEventListener('click', stopMusic)
